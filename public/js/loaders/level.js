cf8dcfb67161ce241dc2afe5e38fe011b8d851cd
hato72 <hatotori72@gmail.com>
2024-03-27 23:41:50 +0900

    init

--- public/js/loaders/level.js
+++ public/js/loaders/level.js
@@ -20,7 +20,12 @@ function setupBackgrounds(levelSpec,level,backgroundSprites){
 }
 
 function setupEntities(levelSpec,level,entityFactory){
-    
+    // levelSpec.entities.forEach(({name,pos:[x,y]}) =>{
+    //     const createEntity = entityFactory[name];
+    //     const entity = createEntity();
+    //     entity.pos.set(x,y);
+    //     level.entities.add(entity);
+    // });
 
     const spriteLayer = createSpriteLayer(level.entities);
     level.comp.layers.push(spriteLayer);
